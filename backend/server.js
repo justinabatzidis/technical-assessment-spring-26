101fde70be97322c6f810ffe5696c6a27ba640aa
Justin Abatzidis <justinabatzidis@crc-dot1x-nat-10-239-209-11.bu.edu>
2025-12-02 01:58:59 -0500

    finalize backend connection and security check

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,7 @@ import 'dotenv/config';
 const app = express();
 app.use(cors());
 app.use(express.json());
-const MONGODB_URI = process.env.MONGODB_URI;
-mongoose.connect(MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("✅ Connected to MongoDB!"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 const PollSchema = new mongoose.Schema({
